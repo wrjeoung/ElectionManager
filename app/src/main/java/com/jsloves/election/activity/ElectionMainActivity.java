007855f48d7d0ea584268a36a5c3d5c37a7a54d8
juhyukkim <kimjuggang@gmail.com>
2015-08-05 16:58:28 +0900

    메인메뉴 죽는거 수정

--- app/src/main/java/com/jsloves/election/activity/ElectionMainActivity.java
+++ app/src/main/java/com/jsloves/election/activity/ElectionMainActivity.java
@@ -1,10 +1,7 @@
 package com.jsloves.election.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by wooram.jeong on 2015-07-07.
@@ -16,12 +13,7 @@ public class ElectionMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
-        Button btn_area_search = (Button) findViewById(R.id.btn_area_search);
-
-        /**
-         * Handling all button click events
-         * */
-
+        /*Button btn_area_search = (Button) findViewById(R.id.btn_area_search);
         // Listening to News Feed button click
         btn_area_search.setOnClickListener(new View.OnClickListener() {
 
@@ -31,6 +23,6 @@ public class ElectionMainActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(), SearchActivity.class);
                 startActivity(i);
             }
-        });
+        });*/
     }
 }
